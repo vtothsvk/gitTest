3f1b5ab7cce571290572bd1889fa1d3da71bc93e
vtothsvk <52499237+vtothsvk@users.noreply.github.com>
2020-11-16 11:29:11 -0500

    Add files via upload

---
+++ gitTest.ino
@@ -0,0 +1,12 @@
+#include <ESP8266WiFi.h>
+
+void setup(){
+    Serial.begin(115200);
+    Serial.println("siz maj bit!");
+    Serial.println("akafuka");
+}
+
+void loop(){
+    delay(100);
+    Serial.println("newBranch");
+}

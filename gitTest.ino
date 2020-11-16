b19a45aaf8eaa2f928a130c7caec1bd4a316f71f
Dave814 <davidhecko@gmail.com>
2020-11-16 18:37:48 +0100

    first commit

--- gitTest.ino
+++ gitTest.ino
@@ -4,7 +4,7 @@ void setup(){
     Serial.begin(115200);
     Serial.println("siz maj bit!");
     Serial.println("akafuka");
-    Serial.println("new Message");
+    Serial.println("simon");
     Serial.println("newBranch local");
 }
 
